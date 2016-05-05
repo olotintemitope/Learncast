02d5a155780ae10b16bff395fa717a48dbb18b6b
andela-tolotin <temitope.olotin@andela.com>
2016-05-05 12:33:36 +0100

    [Feature] Add  cascade delete

---
+++ database/migrations/2016_05_05_112644_addDeleteCascadeToCategoryConstraint.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddDeleteCascadeToCategoryConstraint extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('categories', function (Blueprint $table) {
+            $table->integer('user_id')->unsigned()->default(1);
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('categories', function (Blueprint $table) {
+             $table->dropColumn('user_id');
+        });
+    }
+}
