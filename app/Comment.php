ea7556cc76950d21a872573c89c0db902fa224c2
andela-tolotin <temitope.olotin@andela.com>
2016-05-06 16:29:03 +0100

    [Feature #119086149] Create comment model and add relationship

---
+++ app/Comment.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    protected $fillable = ['comment', 'user_id', 'video_id'];
+
+    public function video()
+    {
+        return $this->belongsTo('App\Video');
+    }
+}
