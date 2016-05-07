1128a7b3216a9bdcd4af86d24a165a872a374fb5
andela-tolotin <temitope.olotin@andela.com>
2016-05-07 11:07:41 +0100

    [Feature #119086149] Add data attritube to button

--- resources/views/main/includes/view_video_details.blade.php
+++ resources/views/main/includes/view_video_details.blade.php
@@ -17,12 +17,12 @@
     @if (Auth::check())
     <li>
      <button type="button" class="btn btn-primary btn-sm favourites" id="{{ $video->id }}" data-user="{{ Auth::user()->id }}" data-fav="{{ $video->favourites }}"> 
-     <i class="fa fa-thumbs-up"> {{ $video->favourites }} </i> 
-    </button>
-  </li>
-  @endif
-</ul>
-<p> {{ $video->description }} </p>
+       <i class="fa fa-thumbs-up"> {{ $video->favourites }} </i> 
+     </button>
+   </li>
+   @endif
+ </ul>
+ <p> {{ $video->description }} </p>
 </div>
 </div>
 </div>
@@ -36,15 +36,33 @@
           <div class="media-body">
            <div class="media">
             <a class="pull-left" href="#">
-              <img class="media-object img-circle " src="{{ URL::to('/') }}/images/user.jpg">
+              <img class="media-object img-circle" src="http://res.cloudinary.com/dfdws9cpx/image/upload/v1462553003/ltn8f7ede007xvxeqgb9.jpg">
             </a>
             <div class="media-body">
               Donec sit amet ligula enim. Duis vel condimentum massa.
               Donec sit amet ligula enim. Duis vel condimentum massa.Donec sit amet ligula enim. 
               Duis vel condimentum massa.
               Donec sit amet ligula enim. Duis vel condimentum massa.
-              <br>
-              <small class="text-muted">Alex Deo | 23rd June at 5:00pm</small>
+              <br><br>
+              <small class="text-muted">Alex Deo</small>
+              <hr>
+            </div>
+          </div>
+        </div>
+      </li> 
+      <li class="media">
+          <div class="media-body">
+           <div class="media">
+            <a class="pull-left" href="#">
+              <img class="media-object img-circle" src="http://res.cloudinary.com/dfdws9cpx/image/upload/v1462553003/ltn8f7ede007xvxeqgb9.jpg">
+            </a>
+            <div class="media-body">
+              Donec sit amet ligula enim. Duis vel condimentum massa.
+              Donec sit amet ligula enim. Duis vel condimentum massa.Donec sit amet ligula enim. 
+              Duis vel condimentum massa.
+              Donec sit amet ligula enim. Duis vel condimentum massa.
+              <br><br>
+              <small class="text-muted">Alex Deo</small>
               <hr>
             </div>
           </div>
@@ -52,14 +70,22 @@
       </li>    
     </ul>
   </div>
+  @if (Auth::check())
   <div class="panel-footer">
+  <div class="preloader-wrapper small active">
+      <img src="{{ URL::to('/') }}/images/preloader.gif" title="preloader" alt="preloader">
+   </div>
+   <form method="POST" id="comment_form">
+     {!! csrf_field() !!}
    <div class="input-group">
-    <input type="text" class="form-control" placeholder="Enter Message">
+    <input type="text" class="form-control" placeholder="Enter Message" id="comment">
     <span class="input-group-btn">
-     <button class="btn btn-primary" type="button">SEND</button>
-   </span>
+      <button class="btn btn-primary" type="button" id="send" data-user="{{ Auth::user()->id }}" data-video="{{ $video->id }}" data-avatar="{{ Auth::user()->picture_url }}" data-username = "{{ ucwords(Auth::user()->username) }}">SEND</button>
+    </span>
  </div>
+ </form>
 </div>
+@endif
 </div>
 </div>
 <div class="col-lg-4">
@@ -68,7 +94,7 @@
   <div class="list_videos ">
     <div class="video_thumbnail">
      <a class="pull-left" href="#">
-      <img class="media-object img-circle " src="{{ URL::to('/') }}/images/user.jpg">
+      <img class="media-object" src="{{ URL::to('/') }}/images/user.jpg">
     </a>
   </div>
   <div class="video_info bg-white">
@@ -80,7 +106,7 @@
 <div class="list_videos">
  <div class="video_thumbnail">
    <a class="pull-left" href="#">
-    <img class="media-object img-circle " src="{{ URL::to('/') }}/images/user.jpg">
+    <img class="media-object" src="{{ URL::to('/') }}/images/user.jpg">
   </a>
 </div>
 <div class="video_info bg-white">
