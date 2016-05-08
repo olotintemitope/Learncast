8e6f003bcf558ee2206da5b3dad6c5a0c512aff1
andela-tolotin <temitope.olotin@andela.com>
2016-05-08 18:19:23 +0100

    [Feature #119147179] Create a new auth for video category

---
+++ app/Http/Middleware/AuthVideoCategory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Auth;
+use Closure;
+
+class AuthVideoCategory
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (Auth::user()->role_id === 1) {
+            abort(401);
+        }
+
+        return $next($request);
+    }
+}
