76d54403d563dd153a9c086483af08b71c4301d2
andela-tolotin <temitope.olotin@andela.com>
2016-05-09 17:01:35 +0100

    [Feature #119179949] Assert that user was created successfully, assert that user already exist

--- tests/UserSignUpTest.php
+++ tests/UserSignUpTest.php
@@ -1,6 +1,5 @@
 <?php
 
-
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 
@@ -8,17 +7,57 @@ class UserSignUpTest extends TestCase
 {
     use DatabaseTransactions;
 
-    public function testCheckAllFieldsEmpty()
+    public function testThatUserSignUpWasSuccessful()
     {
-        $this->WithoutMiddleware();
+        Session::start();
 
         $user = factory('App\User')->create();
 
         $response = $this->actingAs($user)
-           ->call('POST', 'auth/register', [
-               'username'    => 'lytopze',
-               'email'       => 'lytopze@gmail.com',
-               'password'    => 'tope0852',
-           ]);
+        ->call('POST', 'auth/register', [
+            'username'       => 'laztopaz',
+            'email'          => 'laztopaz@phptesting.unit',
+            'password'       => bcrypt(str_random(10)),
+            'profile_bio'    => 'I am a cool dude',
+            'role_id'        => 1,
+            'picture_url'    => 'https://en.gravatar.com/userimage/102347280/b3e9c138c1548147b7ff3f9a2a1d9bb0.png?size=200',
+
+        ]);
+
+        $output  =  (array) json_decode($response->getContent());
+
+        $this->assertEquals($output['message'], 'User created successful');
+        $this->assertEquals($output['statuscode'], 201);
+    }
+
+
+    public function testThatUserAlreadyExists()
+    {
+        Session::start();
+
+        $user = factory('App\User')->create([
+            'username'       => 'laztopaz',
+            'email'          => 'laztopaz@phptesting.unit',
+            'password'       => bcrypt(str_random(10)),
+            'profile_bio'    => 'I am a cool dude',
+            'role_id'        => 1,
+            'picture_url'    => 'https://en.gravatar.com/userimage/102347280/b3e9c138c1548147b7ff3f9a2a1d9bb0.png?size=200',
+        ]);
+
+        $response = $this->actingAs($user)
+        ->call('POST', 'auth/register', [
+            'username'       => 'laztopaz',
+            'email'          => 'laztopaz@phptesting.unit',
+            'password'       => bcrypt(str_random(10)),
+            'profile_bio'    => 'I am a cool dude',
+            'role_id'        => 1,
+            'picture_url'    => 'https://en.gravatar.com/userimage/102347280/b3e9c138c1548147b7ff3f9a2a1d9bb0.png?size=200',
+
+        ]);
+
+        $output  =  (array) json_decode($response->getContent());
+
+        $this->assertEquals($output['message'], 'User already exist');
+        $this->assertEquals($output['statuscode'], 400);
     }
 }
