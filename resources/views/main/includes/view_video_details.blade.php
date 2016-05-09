c2a02ad5d3d751f1b619724cad05b65e5a0f3958
andela-tolotin <temitope.olotin@andela.com>
2016-05-09 23:39:28 +0100

    [Feature #119233901] Add menu option to comments

--- resources/views/main/includes/view_video_details.blade.php
+++ resources/views/main/includes/view_video_details.blade.php
@@ -32,44 +32,59 @@
      <div class="panel-heading">RECENT COMMENT HISTORY</div>
      <div class="panel-body">
        <ul class="media-list">
-       @if (count($video->comments))
-       @foreach($video->comments as $comment)
-        <li class="media">
+         @if (count($video->comments))
+         @foreach($video->comments as $comment)
+         <li class="media">
           <div class="media-body">
            <div class="media">
             <a class="pull-left" href="#">
               <img class="media-object img-circle" src="{{ $comment->user->picture_url }}">
             </a>
             <div class="media-body">
-              {{ $comment->comment }}
-              <br><br>
-              <small class="text-muted">{{ ucwords($comment->user->username) }} | posted {{ Carbon\Carbon::createFromTimeStamp(strtotime($comment->created_at))->diffForHumans() }}</small>
-              <hr>
+             <!-- Single button -->
+             @if (Auth::check() && Auth::user()->id == $comment->user_id)
+             <div class="btn-group pull-right">
+              <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <i class="glyphicon glyphicon-option-vertical"></i>
+              </button>
+              <ul class="dropdown-menu t-menu">
+                <li><a href="#"><i class="glyphicon glyphicon-pencil edit-comment"></i> Edit</a></li>
+                <li role="separator" class="divider"></li>
+                <li><a href="#"><i class="glyphicon glyphicon-trash delete-comment"></i> Delete</a></li>
+              </ul>
             </div>
+            @endif
+            {{ $comment->comment }}
+            <br><br>
+            <small class="text-muted">{{ ucwords($comment->user->username) }} | posted {{ Carbon\Carbon::createFromTimeStamp(strtotime($comment->created_at))->diffForHumans() }}</small>
+            <hr>
           </div>
         </div>
-      </li> 
-       @endforeach
-       @else
-       <li><h4 align="center" class="video_category text-danger">Oops! comments are not available for display!</h4></li>
-       @endif
-    </ul>
-  </div>
-  @if (Auth::check())
-  <div class="panel-footer">
+      </div>
+    </li> 
+    @endforeach
+    @else
+    <li>
+       <h4 align="center" class="video_category text-danger">Oops! comments are not available for display!</h4>
+    </li>
+    @endif
+  </ul>
+</div>
+@if (Auth::check())
+<div class="panel-footer">
   <div class="preloader-wrapper small active">
-      <img src="{{ URL::to('/') }}/images/preloader.gif" title="preloader" alt="preloader">
-   </div>
-   <form method="POST" id="comment_form">
-     {!! csrf_field() !!}
+    <img src="{{ URL::to('/') }}/images/preloader.gif" title="preloader" alt="preloader">
+  </div>
+  <form method="POST" id="comment_form">
+   {!! csrf_field() !!}
    <div class="input-group">
     <!-- <input type="text" class="form-control" placeholder="Enter Message" id="comment"> -->
     <textarea class="form-control" placeholder="Enter Message" id="comment"></textarea>
     <span class="input-group-btn">
       <button class="btn btn-primary clear-fix" type="button" id="send" data-user="{{ Auth::user()->id }}" data-video="{{ $video->id }}" data-avatar="{{ Auth::user()->picture_url }}" data-username = "{{ ucwords(Auth::user()->username) }}">SEND</button>
     </span>
- </div>
- </form>
+  </div>
+</form>
 </div>
 @endif
 </div>
