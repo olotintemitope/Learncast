13dcf36e0b0a2ede55b9ebe344d7d4987d92c047
andela-tolotin <temitope.olotin@andela.com>
2016-05-09 23:38:22 +0100

    [Feature #119233901] Import more fonts

--- resources/views/main/includes/header.blade.php
+++ resources/views/main/includes/header.blade.php
@@ -6,7 +6,6 @@
     <meta name="description" content="This website offers video tutorials accross various learning fields at no cost and its accessible for learning anywhere, anytime 24/7" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="generator" content="Learncast">
-    <!-- <link rel="stylesheet" href="{{ URL::asset('css/materialize.min.css') }}"> -->
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" />
     <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" />
     <link href="//cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.1/animate.min.css" rel="stylesheet" />
