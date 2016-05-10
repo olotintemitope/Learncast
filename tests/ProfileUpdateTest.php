01ad5793655c33ef51ecd50bc5507beb4feac763
andela-tolotin <temitope.olotin@andela.com>
2016-05-10 22:45:19 +0100

    [Feature #119324819] Fix line indentation

--- tests/ProfileUpdateTest.php
+++ tests/ProfileUpdateTest.php
@@ -30,6 +30,19 @@ class ProfileUpdateTest extends TestCase
             ->see('The profile bio field is required.');
     }
 
+    public function testThatUserProfileWasNotUpdated()
+    {
+        $user = factory('App\User')->create();
+
+        $this->actingAs($user)
+            ->visit('/dashboard/profile')
+            ->type('lytopz', 'username')
+            ->type('lytopz@gmail.com', 'email')
+            ->type('I am a cool dude ', 'profile_bio')
+            ->press('Update')
+            ->see('The profile bio field is required.');
+    }
+
     public function testThatSomeFieldsAreMissing()
     {
         $user = factory('App\User')->create();
