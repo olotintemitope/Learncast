f3eb898975f9a2da39365ee8fa2572bbfe0c298f
andela-tolotin <temitope.olotin@andela.com>
2016-05-12 11:44:19 +0100

    [Feature #119422485] Assert that search results was return and Assert that no results found

---
+++ tests/SearchVideoTest.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class SearchVideoTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    public function testThatASearchKeywordReturnAResult()
+    {
+        $video = factory('App\Video', 5)->create();
+
+        $this->visit('/search')
+        ->type($video->first()->title, 'q')
+        ->press("search")
+        ->see($video->first()->title)
+        ->see($video->first()->description);
+
+    }
+
+    public function testThatASearchKeywordReturnRecordNotFound()
+    {
+        $video = factory('App\Video')->create();
+
+        $this->visit('/search')
+        ->type('php', 'q')
+        ->press("search")
+        ->see('0 results found for:');
+    }
+}
