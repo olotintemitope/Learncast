4867e26d5a8bc30f85cc30caa74f9f5bd2ae07eb
andela-tolotin <temitope.olotin@andela.com>
2016-05-12 03:08:47 +0100

    [Feature #119413489] Create a page for search result and display the search result

---
+++ resources/views/main/includes/search_form.blade.php
@@ -0,0 +1,10 @@
+<div class="col-sm-3 col-md-3 pull-right">
+    <form class="navbar-form" role="search" action="/search">
+        <div class="input-group">
+        <input type="text" class="form-control" placeholder="Search" name="q" id="srch-term">
+          <div class="input-group-btn">
+            <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+        </div>
+    </div>
+</form>
+</div>
---
+++ resources/views/main/pages/view_search_result.blade.php
@@ -0,0 +1,7 @@
+@extends('app')
+
+@section('content')
+
+@include('main.includes.search_result_form')
+
+@endsection
