f87edbeb8d3ec223770365983cb40b07cdd991dc
andela-tolotin <temitope.olotin@andela.com>
2016-05-13 18:54:33 +0100

    [chore #119549473] Make the cards the same height

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -16,14 +16,16 @@
         <span class="card-title activator grey-text text-darken-4">
          <i class="material-icons right">more_vert</i></span>
          <div class="row" style="margin-right:3%;">
+               
+
                 <div class="col-lg-3 col-md-6">
-                    <div class="card card-inverse card-danger">
-                        <div class="card-block bg-danger">
+                    <div class="card card-inverse card-primary">
+                        <div class="card-block bg-primary">
                             <div class="rotate">
                                 <i class="fa fa-heart-o fa-4x"></i>
                             </div>
                             <h6 class="text-uppercase">Favourites</h6>
-                            <h6 class="display-1">{{ count($favourites) }}</h6>
+                            <h4 class="display-1">{{ count($favourites) }}</h4>
                         </div>
                     </div>
                 </div>
