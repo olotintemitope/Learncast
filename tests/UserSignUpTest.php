b7b8d142d50cd6c35b84ef913c1e06d013180717
andela-tolotin <temitope.olotin@andela.com>
2016-05-13 23:27:20 +0100

    [chore #119562043] Assert that the user signup via Oauth

--- tests/UserSignUpTest.php
+++ tests/UserSignUpTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use Laravel\Socialite\Facades\Socialite; 
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class UserSignUpTest extends TestCase
@@ -56,5 +57,31 @@ class UserSignUpTest extends TestCase
 
         $this->assertEquals($output['message'], 'User already exist');
         $this->assertEquals($output['statuscode'], 400);
+    }
+
+    public function testThatUserSignUpUsingOauth()
+    {
+        $provider = Mockery::mock('Laravel\Socialite\Contracts\Provider');
+        $provider->shouldReceive('redirect')->andReturn('Redirected');
+        $providerName = class_basename($provider);
+        $socialAccount = factory('LearnCast\User')->create(['provider' => $providerName]);
+        
+        $abstractUser = Mockery::mock('Laravel\Socialite\Two\User');
+        $abstractUser->shouldReceive('getId')
+            ->andReturn($socialAccount->provider_user_id)
+            ->shouldReceive('getEmail')
+            ->andReturn(str_random(10).'@noemail.app')
+            ->shouldReceive('getNickname')
+            ->andReturn('Olotin Temitope')
+            ->shouldReceive('getAvatar')
+            ->andReturn('https://en.gravatar.com/userimage/102347280/b3e9c138c1548147b7ff3f9a2a1d9bb0.png?size=200');
+
+        $provider = Mockery::mock('Laravel\Socialite\Contracts\Provider');
+        $provider->shouldReceive('user')->andReturn($abstractUser);
+       
+        Socialite::shouldReceive('driver')->with('facebook')->andReturn($provider);
+
+        $this->visit('/auth/facebook/callback')
+        ->seePageIs('/');
     }
 }
