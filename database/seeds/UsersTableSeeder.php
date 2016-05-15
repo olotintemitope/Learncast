56c7f2946340e61ef9c446c9b768a18480ab7a4e
andela-tolotin <temitope.olotin@andela.com>
2016-05-15 20:12:52 +0100

    [chore #119599057 Add favourites to seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -15,5 +15,7 @@ class UsersTableSeeder extends Seeder
         factory(LearnCast\User::class, 1)->create();
         factory(LearnCast\Category::class, 5)->create();
         factory(LearnCast\Video::class, 1)->create();
+        factory(LearnCast\Comment::class, 1)->create();
+        factory(LearnCast\Favourite::class, 1)->create();
     }
 }
