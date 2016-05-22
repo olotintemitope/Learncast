bef31aa61acffdb9d12cab8ab84481b85955743d
andela-tolotin <temitope.olotin@andela.com>
2016-05-22 23:10:38 +0100

    [bug #120046623] Add a color class to icon

--- resources/views/main/includes/video_category_slide.blade.php
+++ resources/views/main/includes/video_category_slide.blade.php
@@ -4,7 +4,7 @@
        @foreach($allCategories as $category)
        <li class="list-group-item"> 
         <a href="/video/category/{{ $category->name }}">
-          <p class="img-circle"><i class="devicon-{{ strtolower($category->name) }}-plain"></i>  {{ $category->name }}</p>
+          <p class="img-circle"><i class="devicon-{{ strtolower($category->name) }}-plain colored"></i>  {{ $category->name }}</p>
         </a>
       </li>
       @endforeach
