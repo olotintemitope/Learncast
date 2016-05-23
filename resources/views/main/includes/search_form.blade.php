19a14845562ae167fe7bd9484ade81eca1039ea6
andela-tolotin <temitope.olotin@andela.com>
2016-05-23 20:02:52 +0100

    [bug #120132793] Fix search button height

--- resources/views/main/includes/search_form.blade.php
+++ resources/views/main/includes/search_form.blade.php
@@ -3,7 +3,7 @@
         <div class="input-group">
         <input type="text" class="form-control" placeholder="Search" name="q" id="srch-term">
           <div class="input-group-btn">
-            <button class="btn btn-primary" style="height:35px;" type="submit" name="search" value="results">
+            <button class="btn btn-primary" style="height:34px;" type="submit" name="search" value="results">
             <i class="glyphicon glyphicon-search"></i></button>
         </div>
     </div>
