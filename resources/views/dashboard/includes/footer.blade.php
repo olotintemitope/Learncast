4a12466b69088f552d742a666320b77f064c6e8f
andela-tolotin <temitope.olotin@andela.com>
2016-05-24 10:43:00 +0100

    [bug #120178911] Fix conflicting jquery  library

--- resources/views/dashboard/includes/footer.blade.php
+++ resources/views/dashboard/includes/footer.blade.php
@@ -1,66 +1,32 @@
 <footer class="main-footer">
-    <div class="pull-right hidden-xs">
-      <b>Version</b> 1.0.0
-    </div>
-    <strong>Copyright &copy; 2016-2017 <a href="http://github.com/andela-tolotin">Temitope Olotin</a>.</strong> All rights
-    reserved.
-  </footer>
+  <div class="pull-right hidden-xs">
+    <b>Version</b> 1.0.0
+  </div>
+  <strong>Copyright &copy; 2016-2017 <a href="http://github.com/andela-tolotin">Temitope Olotin</a>.</strong> All rights
+  reserved.
+</footer>
 <!-- jQuery 2.2.0 -->
 <script src="{{ URL::asset('plugins/jQuery/jQuery-2.2.0.min.js') }}"></script>
 <!-- jQuery UI 1.11.4 -->
-<script src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-<script>
-  $.widget.bridge('uibutton', $.ui.button);
-</script>
-<!-- Bootstrap 3.3.6 -->
-<script src="{{ URL::asset('js/bootstrap.min.js') }}"></script>
-<!-- Morris.js charts -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
-<script src="{{ URL::asset('plugins/morris/morris.min.js') }}"></script>
-<!-- Sparkline -->
-<script src="{{ URL::asset('plugins/sparkline/jquery.sparkline.min.js') }}"></script>
-<!-- jvectormap -->
-<script src="{{ URL::asset('plugins/jvectormap/jquery-jvectormap-1.2.2.min.js') }}"></script>
-<script src="{{ URL::asset('plugins/jvectormap/jquery-jvectormap-world-mill-en.js') }}"></script>
-<!-- jQuery Knob Chart -->
-<script src="{{ URL::asset('plugins/knob/jquery.knob.js') }}"></script>
-<!-- daterangepicker -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.11.2/moment.min.js"></script>
-<script src="{{ URL::asset('plugins/daterangepicker/daterangepicker.js') }}"></script>
-<!-- datepicker -->
-<script src="{{ URL::asset('plugins/datepicker/bootstrap-datepicker.js') }}"></script>
-<!-- Bootstrap WYSIHTML5 -->
-<script src="{{ URL::asset('plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js') }}"></script>
-<!-- Slimscroll -->
-<script src="{{ URL::asset('plugins/slimScroll/jquery.slimscroll.min.js') }}"></script>
-<!-- FastClick -->
-<script src="{{ URL::asset('plugins/fastclick/fastclick.js') }}"></script>
-<!-- AdminLTE App -->
-<script src="{{ URL::asset('js/app.min.js') }}"></script>
-<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="{{ URL::asset('js/pages/dashboard.js') }}"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="{{ URL::asset('dist/js/demo.js') }}"></script>
-
-<!--scripts loaded here-->
-<!-- <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script> -->
-<!-- <script src="{{ URL::asset('js/jquery-2.1.1.min.js') }}"></script> -->
-<script src="{{ URL::asset('js/scripts.js') }}"></script>
-<script src="{{ URL::asset('js/jquery.als-1.7.min.js') }} "></script>
-<script src="{{ URL::asset('js/video_category.js') }}"></script>
+<script src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
+<!-- My plugin for video and video categories -->
 <script src="{{ URL::asset('js/videoCategory.js') }}"></script>
 <script src="{{ URL::asset('js/Video.js') }}"></script>
 <script src="{{ URL::asset('js/sweetalert.min.js') }}"></script>
-<script src="{{ URL::asset('js/materialize.min.js') }}"></script>
+
+<!-- Bootstrap 3.3.6 -->
+<script src="{{ URL::asset('js/bootstrap.min.js') }}"></script>
+<!-- AdminLTE App -->
+<script src="{{ URL::asset('js/app.min.js') }}"></script>
+
 <script>
- $(document).ready(function() {
+  $(function() {
+    // More code using $ as alias to jQuery
     $("body").videoCategoryPlugin();
     $("body").videoPlugin();
-    // $(".button-collapse").sideNav();
-    // $('.collapsible').collapsible();
-    // $('select').material_select();
- });
+    //$.widget.bridge('uibutton', $.ui.button);
+  });
 </script>
 </body>
 </html>
