8dc0173caa4d67f17ba225d50e4b71bd20cf5577
andela-tolotin <temitope.olotin@andela.com>
2016-05-27 10:51:42 +0100

    [bug #120190219] Rearrange the video view display

--- resources/views/main/includes/view_video_details.blade.php
+++ resources/views/main/includes/view_video_details.blade.php
@@ -3,90 +3,100 @@
    @if (count($video) > 0)
    <div class="col-lg-12 bg-white video_frame">
      <div class="embed-responsive embed-responsive-16by9">
-      <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/{{ $video->url }}" frameborder="0" allowfullscreen></iframe>
+      <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/{{ $video->url }}?autoplay=1" frameborder="0" allowfullscreen></iframe>
     </div>
-    <h3> {{ ucwords($video->title) }}</h3><p class="pull-right"><em>Created by </em><a href="#" ><strong> {{ ucwords($video->user->username) }}</strong></a></p>
-    <div class="video_details">
-     <ul class="list-inline">
-      <li>
-       <button type="button" class="btn btn-primary btn-sm views"> 
-        <i class="fa fa-eye"> {{ $video->views }}  </i> 
-      </button>
-    </li>
-    <li><button type="button" class="btn btn-primary btn-sm comments"> <i class="fa fa-comment"> {{ count($video->comments) }}</i>
-    </li>
-    <li>
-      <form>
-        @if (Auth::check())
-        <button type="button" class="btn btn-primary btn-sm favourites" id="{{ $video->id }}" data-user="{{ Auth::user()->id }}" data-fav="{{ $video->favourites }}"> 
-         <i class="fa fa-heart"> {{ $video->favourites }} </i> 
-       </button>
-       @else
-       <button type="button" class="btn btn-primary btn-sm favourites" id="{{ $video->id }}" data-user="" data-fav="{{ $video->favourites }}"> 
-         <i class="fa fa-thumbs-up"> {{ $video->favourites }} </i> 
-       </button>
-       @endif
-     </form>
-   </li>
- </ul>
- <p> {{ $video->description }} </p>
-</div>
-</div>
+    
+  </div>
 </div>
 <div class="row">
   <div class="col-lg-8">
-    <div class="panel panel-primary">
-     <div class="panel-heading">RECENT COMMENT HISTORY</div>
-     <div class="panel-body">
-       <ul class="media-list">
-         @if (count($video->comments))
-         @foreach($video->comments as $comment)
-         <li class="media">
-          <div class="media-body">
-           <div class="media">
-            <a class="pull-left" href="#">
-              <img class="media-object img-circle" src="{{ $comment->user->picture_url }}">
-            </a>
-            <div class="media-body">
-             <!-- Single button -->
-             @if (Auth::check() && Auth::user()->id == $comment->user_id)
-             <div class="btn-group pull-right">
-              <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <i class="glyphicon glyphicon-option-vertical"></i>
-              </button>
-              <ul class="dropdown-menu t-menu">
-                <li  class= "edit-comment" id="{{ $comment->id }}"><a href="#"><i class="glyphicon glyphicon-pencil"></i> Edit</a></li>
-                <li role="separator" class="divider"></li>
-                <li class="delete-comment" id="{{ $comment->id }}"><a href="#"><i class="glyphicon glyphicon-trash" ></i> Delete</a></li>
-              </ul>
-            </div>
-            @else
-            <div class="btn-group pull-right">
-              <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <i class="glyphicon glyphicon-option-vertical"></i>
-              </button>
-              <ul class="dropdown-menu t-menu">
-                <li  class= "" id=""><a href=""><i class="glyphicon glyphicon-info"></i>Can't edit</a></li>
-              </ul>
-            </div>
-            @endif
-            <div class="comment" id="comment{{ $comment->id }}">{{ $comment->comment }}</div>
-            <div class="comment_form"></div>
-            <br><br>
-            <small class="text-muted">{{ ucwords($comment->user->username) }} | posted {{ Carbon\Carbon::createFromTimeStamp(strtotime($comment->created_at))->diffForHumans() }}
-            </small>
-            <hr>
-          </div>
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="video-description"> 
+          <div class="video_details">
+           <ul class="list-inline">
+            <li>
+             <button type="button" class="btn btn-primary btn-sm views"> 
+              <i class="fa fa-eye"> {{ $video->views }}  </i> 
+            </button>
+          </li>
+          <li><button type="button" class="btn btn-primary btn-sm comments"> <i class="fa fa-comment"> {{ count($video->comments) }}</i>
+          </li>
+          <li>
+            <form>
+              @if (Auth::check())
+              <button type="button" class="btn btn-primary btn-sm favourites" id="{{ $video->id }}" data-user="{{ Auth::user()->id }}" data-fav="{{ $video->favourites }}"> 
+               <i class="fa fa-heart"> {{ $video->favourites }} </i> 
+             </button>
+             @else
+             <button type="button" class="btn btn-primary btn-sm favourites" id="{{ $video->id }}" data-user="" data-fav="{{ $video->favourites }}"> 
+               <i class="fa fa-thumbs-up"> {{ $video->favourites }} </i> 
+             </button>
+             @endif
+           </form>
+         </li>
+       </ul>
+     </div>
+     <h3> {{ ucwords($video->title) }} </h3> 
+     <p class="video-author">
+       <img class="media-object img-circle pull-left" src="{{ $video->user->picture_url }}"><a href="#" ><strong> {{ ucwords($video->user->username) }}</strong></a>
+     </p>
+     <p style="clear:both;">{{ $video->description }} </p>
+   </div>
+ </div>
+</div>
+<div class="panel panel-primary">
+ <div class="panel-heading">RECENT COMMENT HISTORY</div>
+ <div class="panel-body">
+   <ul class="media-list">
+     @if (count($video->comments))
+     @foreach($video->comments as $comment)
+     <li class="media">
+      <div class="media-body">
+       <div class="media">
+        <a class="pull-left" href="#">
+          <img class="media-object img-circle" src="{{ $comment->user->picture_url }}">
+        </a>
+        <div class="media-body">
+         <!-- Single button -->
+         @if (Auth::check() && Auth::user()->id == $comment->user_id)
+         <div class="btn-group pull-right">
+          <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <i class="glyphicon glyphicon-option-vertical"></i>
+          </button>
+          <ul class="dropdown-menu t-menu">
+            <li  class= "edit-comment" id="{{ $comment->id }}"><a href="#"><i class="glyphicon glyphicon-pencil"></i> Edit</a></li>
+            <li role="separator" class="divider"></li>
+            <li class="delete-comment" id="{{ $comment->id }}"><a href="#"><i class="glyphicon glyphicon-trash" ></i> Delete</a></li>
+          </ul>
         </div>
+        @else
+        <div class="btn-group pull-right">
+          <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <i class="glyphicon glyphicon-option-vertical"></i>
+          </button>
+          <ul class="dropdown-menu t-menu">
+            <li  class= "" id=""><a href=""><i class="glyphicon glyphicon-info"></i>Can't edit</a></li>
+          </ul>
+        </div>
+        @endif
+        <div class="comment" id="comment{{ $comment->id }}">{{ $comment->comment }}</div>
+        <div class="comment_form"></div>
+        <br><br>
+        <small class="text-muted">{{ ucwords($comment->user->username) }} | posted {{ Carbon\Carbon::createFromTimeStamp(strtotime($comment->created_at))->diffForHumans() }}
+        </small>
+        <hr>
       </div>
-    </li> 
-    @endforeach
-    @else
-    <li>
-     <h4 align="center" class="video_category text-danger">Comments are not available for display!</h4>
-   </li>
-   @endif
- </ul>
+    </div>
+  </div>
+</li> 
+@endforeach
+@else
+<li>
+ <h4 align="center" class="video_category text-danger">Comments are not available for display!</h4>
+</li>
+@endif
+</ul>
 </div>
 @if (Auth::check())
 <div class="panel-footer" style="margin-bottom: 20px;">
